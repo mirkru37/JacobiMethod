037c01849df54e9657711ceebafb5a7718ecc629
Roman <mirkru37@gmail.com>
2021-10-04 19:15:41 +0300

    Finish

---
+++ Jacobi.py
@@ -0,0 +1,31 @@
+from fractions import Fraction
+
+
+def iteration(matrix, x_i, accuracy, steps):
+    new_x = []
+    for i, var in enumerate(matrix):
+        new_x.append(var[-1])
+        for j, v in enumerate(var[:-1]):
+            if j != i:
+                new_x[i] += v * x_i[j]
+        new_x[i] /= matrix[i][i]
+    steps.append(new_x)
+    if max([abs(new_x[i] - x_i[i]) for i in range(len(x_i))]) <= accuracy:
+        return new_x
+    else:
+        return iteration(matrix, new_x, accuracy, steps)
+
+
+def jacobi(matrix, accuracy=0.0001):
+    x = []
+    steps = []
+    for i, var in enumerate(matrix):
+        sum_ = 0
+        for j in var[:-1]:
+            if j != i:
+                sum_ += j
+        if abs(matrix[i][i]) < sum_:
+            return None, None
+        x.append(Fraction(var[-1] / var[i]))
+    x = iteration(matrix, x, accuracy, steps)
+    return x, steps
---
+++ main.py
@@ -0,0 +1,20 @@
+import Jacobi
+import Input
+import Menu
+
+
+# print(x)
+matrix = Menu.menu(Menu.input_matrix)
+print("Your matrix")
+for i in matrix:
+    print(i)
+n = Input.number("Input accuracy: ")
+x, steps = Jacobi.jacobi(matrix, n)
+length = int(len((str(n).split('.')[-1])))
+if x is not None:
+    print("Jacobi result:", [round(float(i), length) for i in x])
+    print("Steps:")
+    for i in steps:
+        print([round(float(s), length) for s in i])
+else:
+    print("Speed is to low, please change coefficients!!")
